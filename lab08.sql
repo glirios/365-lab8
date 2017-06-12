ea7b36d54820dafb7cf44b345f4e1c9b6fa019bf
glirios <lirios311@gmail.com>
2017-06-12 12:23:53 -0700

    some queries

---
+++ lab08.sql
@@ -0,0 +1,52 @@
+-- q2
+SELECT Name
+FROM Securities s, Prices p 
+WHERE s.ticker = p.ticker AND
+	YEAR(p.day) = 2016
+GROUP BY p.ticker 
+ORDER BY Sum(p.volume) DESC 
+LIMIT 10;
+
+-- q1 part 3
+SELECT SUM(s.ticker)
+FROM Securities s, 
+	(SELECT ticker, open 
+	FROM Prices 
+	WHERE day = "2015-12-31"
+	GROUP BY ticker) p1, 
+	(SELECT ticker, close
+	FROM Prices 
+	WHERE day = "2016-12-30"
+	GROUP BY ticker) p2
+WHERE s.ticker = p1.ticker and s.ticker = p2.ticker and p1.open < p2.close;
+
+
+-- subquery of 3
+SELECT DISTINCT YEAR(day) FROM Prices;
+
+
+-- q4
+SELECT Name, s.ticker, p2.price, p1.price, (p2.price - p1.price) / p1.price
+FROM 
+	(SELECT ticker, AVG(close) AS "price"
+	FROM Prices
+	WHERE YEAR(day) = 2016 AND MONTH(day) = 1
+	GROUP BY ticker) p1,
+	 (SELECT ticker, AVG(close) AS "price"
+	 FROM Prices
+	 WHERE YEAR(day) = 2016 AND MONTH(day) = 12
+	 GROUP BY ticker) p2,
+	 Securities s
+WHERE s.ticker = p1.ticker and s.ticker = p2.ticker
+ORDER BY (p2.price - p1.price) / p1.price DESC
+LIMIT 10
+;
+
+-- Individual Queries
+-- Q1
+
+SELECT ticker, MIN(day), MAX(day)
+FROM Prices
+WHERE ticker = <Ticker>;
+
+-- Q2
