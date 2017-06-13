f3152d54eb8856325e7ed861847b43329eb08fbe
R3xKyle <kyle@ringler.com>
2017-06-12 17:24:32 -0700

    Update lab8.java - Put your credentials at top of main
    HTML working

--- lab8.java
+++ lab8.java
@@ -1,9 +1,15 @@
 import java.sql.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class lab8 {
 
 	static Connection conn = null;
-
+   static String filename = "general.html";
+   static String ticker = "General";
+   static FileWriter fw = null;
+   static BufferedWriter bw = null;
 
 	public static void main(String args[])
 	{
@@ -64,8 +70,9 @@ public class lab8 {
 		}
 		*/
 
-
+      startTemplate();
 		generalAnalysis();
+      endTemplate();
 		/*
 		try
 		{
@@ -101,14 +108,17 @@ public class lab8 {
 					+ "AND Month(Day) = 1)";
 			ResultSet result = s1.executeQuery(query);
 			boolean f = result.next();
+         startTable();
+         bw.write("<caption>Total number of securities traded at the "
+            + "start of 2016</caption>\n<tr>\n<th>Total</th>\n</tr>\n");
 			while (f)
 			{
 				int total = result.getInt(1);
-				System.out.println("Total number of securities traded at the"
-						+ " start of 2016");
-				System.out.println(total);
+				bw.write("<tr>\n<td>" + total + "</td>\n</tr>\n");
+				//System.out.println(total);
             f = result.next();
 			}
+         endTable();
 		}
 		catch (Exception ex)
 		{
@@ -125,14 +135,17 @@ public class lab8 {
 					+ "AND Month(Day) = 12)";
 			ResultSet result = s2.executeQuery(query);
 			boolean f = result.next();
+         startTable();
+         bw.write("<caption>Total number of securities traded at the "
+            + "end of 2016</caption>\n<tr>\n<th>Total</th>\n</tr>\n");
 			while (f)
 			{
 				int total = result.getInt(1);
-				System.out.println("Total number of securities traded at the "
-						+ "end of 2016");
-				System.out.println(total);
+				bw.write("<tr>\n<td>" + total + "</td>\n</tr>\n");
+				//System.out.println(total);
             f = result.next();
 			}
+         endTable();
 		}
 		catch (Exception ex)
 		{
@@ -154,14 +167,18 @@ public class lab8 {
 					+ "WHERE x.Ticker = x1.Ticker AND x.Close > x1.Close";
 			ResultSet result = s3.executeQuery(query);
 			boolean f = result.next();
+         startTable();
+         bw.write("<caption>Total number of securities whose prices saw "
+            + "an increase from end of 2015 to end of 2016</caption>\n"
+            + "<tr>\n<th>Total</th>\n</tr>\n");
 			while (f)
 			{
 				int total = result.getInt(1);
-				System.out.println("Total number of securities whose prices saw"
-						+ " an increase from end of 2015 to end of 2016");
-				System.out.println(total);
+				bw.write("<tr>\n<td>" + total + "</td>\n</tr>\n");
+				//System.out.println(total);
             f = result.next();
 			}
+         endTable();
 		}
 		catch (Exception ex)
 		{
@@ -183,14 +200,18 @@ public class lab8 {
 					+ "WHERE x.Ticker = x1.Ticker AND x.Close < x1.Close";
 			ResultSet result = s4.executeQuery(query);
 			boolean f = result.next();
+         startTable();
+         bw.write("<caption>Total number of securities whose prices saw "
+            + "a decrease from end of 2015 to end of 2016</caption>\n"
+            + "<tr>\n<th>Total</th>\n</tr>\n");
 			while (f)
 			{
 				int total = result.getInt(1);
-				System.out.println("Total number of securities whose prices"
-						+ " saw a decrease from end of 2015 to end of 2016");
-				System.out.println(total);
+            bw.write("<tr>\n<td>" + total + "</td>\n</tr>\n");
+				//System.out.println(total);
             f = result.next();
 			}
+         endTable();
 		}
 		catch (Exception ex)
 		{
@@ -206,14 +227,16 @@ public class lab8 {
 					+ " GROUP BY Ticker ORDER BY (Volume) DESC LIMIT 10";
 			ResultSet result = s5.executeQuery(query);
 			boolean f = result.next();
-			System.out.println("Top 10 stocks that were most heavily traded"
-					+ " in 2016");
+         startTable();
+         bw.write("<caption>Top 10 stocks that were most heavily traded"
+            + "</caption>\n<tr>\n<th>Tickers</th>\n</tr>\n");
 			while (f)
 			{
 				String s = result.getString(1);
-				System.out.println(s);
+				bw.write("<tr>\n<td>" + s + "</td>\n</tr>\n");
 				f = result.next();
 			}
+         endTable();
 		}
 		catch (Exception ex)
 		{
@@ -226,10 +249,16 @@ public class lab8 {
 		{
 			Statement s6 = conn.createStatement();
 			query = "SELECT DISTINCT YEAR(Day) FROM Prices";
+         int years[] = new int[7];
+         String absolute[] = new String[5];
+         String relative[] = new String[5];
+         int i = 0;
 			ResultSet result = s6.executeQuery(query);
 			boolean f = result.next();
-			System.out.println("For each year, Top 5 highest performing stocks "
-					+ "for both absolute and relative price increase");
+         startTable();
+         bw.write("<caption>Top 5 highest performing stocks for both "
+            + "absolute and relative price increase</caption>\n<tr>\n"
+            + "<th>Year</th><th>Absolute</th><th>Relative</th></tr>\n");
 			while(f)
 			{
 				int year = result.getInt(1);
@@ -248,12 +277,14 @@ public class lab8 {
 							+ "WHERE x.Ticker = x1.Ticker "
 							+ "ORDER BY (x.Close - x1.Close) DESC LIMIT 5";
 					ResultSet result1 = s7.executeQuery(query);
-					System.out.println("Top 5 absolute for year " + year);
+					//System.out.println("Top 5 absolute for year " + year);
 					boolean f1 = result1.next();
+               i = 0;
 					while (f1)
 					{
 						String s1 = result1.getString(1);
-						System.out.println(s1);
+						absolute[i++] = s1;
+                  //System.out.println(s1);
 						f1 = result1.next();
 					}
 					Statement s8 = conn.createStatement();
@@ -270,12 +301,14 @@ public class lab8 {
 							+ "ORDER BY ((x.Close - x1.Close)/ x1.Close) DESC"
 							+ " LIMIT 5";
 					ResultSet result2 = s8.executeQuery(query);
-					System.out.println("Top 5 relative for year " + year);
+					//System.out.println("Top 5 relative for year " + year);
 					boolean f2 = result2.next();
+               i = 0;
 					while (f2)
 					{
 						String s2 = result2.getString(1);
-						System.out.println(s2);
+                  relative[i++] = s2;
+						//System.out.println(s2);
 						f2 = result2.next();
 					}
 				}
@@ -285,8 +318,15 @@ public class lab8 {
 							+ "for year " + year);
 					System.out.println(ex);
 				}
+            bw.write("<tr><td rowspan=\"6\">" + year + "</td></tr>\n");
+            for (i = 0; i < 5; i++)
+            {
+               bw.write("<tr><td>" + absolute[i] + "</td>\n");
+               bw.write("<td>" + relative[i] + "</td>\n</tr>\n");
+            }
 				f = result.next();
 			}
+         endTable();
 		}
 		catch (Exception ex)
 		{
@@ -309,11 +349,14 @@ public class lab8 {
 					+ "ORDER BY ((x1.Avg - x.Avg) / x.Avg) DESC LIMIT 10";
 			ResultSet result = s9.executeQuery(query);
 			boolean f = result.next();
+         startTable();
+         bw.write("<caption>10 stocks to watch</caption>\n<tr>\n"
+            + "<th>Ticker</th></tr>\n");
 			System.out.println("10 Stocks to watch");
 			while (f)
 			{
 				String s = result.getString(1);
-				System.out.println(s);
+            bw.write("<tr>\n<td>" + s + "</td>\n</tr>\n");
 				f = result.next();
 			}
 		}
@@ -330,13 +373,16 @@ public class lab8 {
 			query = "SELECT DISTINCT Sector FROM Securities";
 			ResultSet result = s10.executeQuery(query);
 			boolean f = result.next();
-			System.out.println("Assessments for different sectors");
+			//System.out.println("Assessments for different sectors");
+         startTable();
+         bw.write("<caption>Assessments of different sectors</caption>\n<tr>\n"
+            + "<th>Sector</th><th>Assessment</th></tr>\n");
 			while (f)
 			{
 				String sector = result.getString(1);
 				if (!"Telecommunications Services".equals(sector))
 				{
-					System.out.println("For Sector " + sector);
+					//System.out.println("For Sector " + sector);
 					try
 					{
 						Statement s11 = conn.createStatement();
@@ -356,32 +402,34 @@ public class lab8 {
 								+ "AND x.Ticker = x1.Ticker) y";
 						ResultSet result1 = s11.executeQuery(query);
 						boolean f1 = result1.next();
+                  String rank = "";
 						while (f1)
 						{
 							float avg = result1.getFloat(1);
 							if (avg < .02)
 							{
-								System.out.println("Unsafe");
+								rank = "Unsafe";
 							}
 							else if (avg < .05)
 							{
-								System.out.println("Stationary");
+								rank = "Stationary";
 							}
 							else if (avg < .10)
 							{
-								System.out.println("Showing resilience");
+								rank = "Showing resilience";
 							}
 							else if (avg < .25)
 							{
-								System.out.println("Secure");
+								rank = "Secure";
 							}
 							else
 							{
-								System.out.println("Significantly"
-										+ " outperforming");
+							   rank = "Significantly outperforming";
 							}
 							f1 = result1.next();
 						}
+                  bw.write("<tr>\n<td>" + sector + "</td>\n<td>" + rank
+                   + "</td></tr>\n");
 					}
 					catch (Exception ex)
 					{
@@ -401,4 +449,78 @@ public class lab8 {
 		}
 
 	}
+
+
+   public static void startTemplate()
+   {
+      try
+      {
+         fw = new FileWriter(filename);
+         bw = new BufferedWriter(fw);
+      }
+      catch (Exception ex)
+      {
+         ex.printStackTrace();
+         System.exit(0);
+      }
+
+      try
+      {
+         bw.write("<!DOCTYPE html>\n");
+         bw.write("<html>\n");
+         bw.write("<head><style>\ntable, th, td {\nborder: 1px solid black;" +
+            "\nborder-collapse: collapse;\n}\nth, td {\npadding: 5px;\n" +
+            "text-align: center;\n}\n</style><center>Stock Analysis for " +
+            ticker + "</center></head>\n");
+         bw.write("<body>\n");
+      }
+      catch (Exception ex)
+      {
+         System.out.println("Failed header");
+         System.out.println(ex);
+         System.exit(0);
+      }
+   }
+
+   public static void endTemplate()
+   {
+      try
+      {
+         bw.write("</body>\n");
+         bw.write("</html>");
+         bw.close();
+         fw.close();
+      }
+      catch (Exception ex)
+      {
+         System.out.println("Failed end template");
+         System.out.println(ex);
+      }
+   }
+
+   public static void startTable()
+   {
+      try
+      {
+         bw.write("<table style=\"width:100%\">\n");
+      }
+      catch (Exception ex)
+      {
+         System.out.println("Failed to write table");
+         System.out.println(ex);
+      }
+   }
+
+   public static void endTable()
+   {
+      try
+      {
+         bw.write("</table>\n");
+      }
+      catch (Exception ex)
+      {
+         System.out.println("Failed to close table");
+         System.out.println(ex);
+      }
+   }
 }
