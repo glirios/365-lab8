ee651a0b3e777046a37bcf2754336a923fce93d4
glirios <lirios311@gmail.com>
2017-06-14 18:12:46 -0700

    ...

--- lab8.java
+++ lab8.java
@@ -473,7 +473,7 @@ public class lab8 {
    public static void individualAnalysis(String ticker)
    {
    		// Query 1
-   		string query = "";
+   		String query = "";
 		try
 		{
 			Statement s1 = conn.createStatement();
